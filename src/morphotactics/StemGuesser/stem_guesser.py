78f64363e6f676a9fd7a6af98f815e85353e1b59
Kalvin Chang <kalvin1204@gmail.com>
2021-04-10 22:19:36 +0800

    add stem guesser FST

---
+++ src/morphotactics/StemGuesser/stem_guesser.py
@@ -0,0 +1,100 @@
+import pynini
+import pywrapfst as fst
+import graphviz
+from IPython.display import SVG, display
+
+class StemGueser:
+  # min_word_constraint: a minimal word constraint expressed as a limited regular expression of phone classes
+  # alphabet (optional): dictionary mapping phone classes to list of symbols; if sigma (.) is used in the regex, alphabet is required
+  # assumes long vowels have been expanded
+  # returns an OpenFst FST corresponding to the regex with classes substituted with symbols
+  # converts a limited PCRE regex (scope, quantification) to an OpenFst FST
+  def __init__(min_word_constraint, alphabet={}):
+    # phone classes could overlap so phones to set first
+    symbols = {symb for symbol_class in alphabet.values() for symb in symbol_class}
+
+    stack = [] # check for matching parens
+    fst = None
+    fst_stack = [] # to be used in union or scope mode
+    regex = min_word_constraint
+
+    # () means scope / grouping - concatenation
+    # [] means match anything inside - union
+    # . means match any character in the alphabet (not including epsilon) - sigma
+    # quantifiers: ?, *, +
+    for i in range(len(regex)):
+      if regex[i] == '[':
+        stack.append(regex[i])
+        fst_stack.append(('union', pynini.accep('')))
+      elif regex[i] == '(':
+        stack.append(regex[i])
+        fst_stack.append(('scope', pynini.accep('')))
+      elif regex[i] == ')':
+        if stack.pop(-1) != '(':
+          raise Exception('Unmatched parentheses')
+        fst_stack[-1] = ('processed', fst_stack[-1][1])
+      elif regex[i] == ']':
+        if stack.pop(-1) != '[':
+          raise Exception('Unmatched brackets')
+        fst_stack[-1] = ('processed', fst_stack[-1][1])
+      elif fst_stack and fst_stack[-1][0] in ['scope', 'union']:
+        if fst_stack[-1][0] == 'scope':
+          # concatenate only the current chars
+          if regex[i] not in alphabet:
+            fst_stack[-1][1].concat(regex[i])
+          else:
+            fst_stack[-1][1].concat(pynini.union(*alphabet[regex[i]]))
+        elif fst_stack[-1][0] == 'union':
+          if fst_stack[-1][1].num_states() == 1:
+            # make sure we don't union with empty string
+            if regex[i] not in alphabet:
+              fst_stack[-1][1].concat(regex[i])
+            else:
+              fst_stack[-1][1].concat(pynini.union(*alphabet[regex[i]]))
+          else:
+            # union only the current chars within the matching parens
+            if regex[i] not in alphabet:
+              fst_stack[-1][1].union(regex[i])
+            else:
+              fst_stack[-1][1].union(pynini.union(*alphabet[regex[i]]))
+      # sigma
+      elif regex[i] == '.':
+        if not alphabet:
+          raise Exception('Alphabet required if regex includes sigma')
+        # make copy each time to avoid state issues
+        sigma = pynini.union(*list(symbols))
+        fst_stack.append(('sigma', sigma))
+      # quantification - perform closure on last FST
+      elif regex[i] == '?':
+        if i == 0:
+          raise Exception('Empty quantification')
+        fst_stack[-1] = (fst_stack[-1][0], pynini.closure(fst_stack[-1][1], 0, 1))
+      elif regex[i] == '*':
+        if i == 0:
+          raise Exception('Empty quantification')
+
+        fst_stack[-1] = (fst_stack[-1][0], pynini.closure(fst_stack[-1][1]))
+        
+        # if the entire regex is a Kleene closure or previous character is sigma, accept empty string too
+        if (len(fst_stack) == 1 and i == len(regex) - 1) or (fst_stack and fst_stack[-1][0] == 'sigma'):
+          fst_stack[-1] = (fst_stack[-1][0], pynini.union(fst_stack[-1][1], ''))
+      elif regex[i] == '+':
+        if i == 0:
+          raise Exception('Empty quantification')
+        fst_stack[-1] = (fst_stack[-1][0], pynini.closure(fst_stack[-1][1], 1))
+      else:
+        if regex[i] not in alphabet:
+          fst_stack.append(('symbol', pynini.accep(regex[i])))
+        else:
+          fst_stack.append(('symbol', pynini.union(*alphabet[regex[i]])))
+    
+    for (_, f) in fst_stack:
+      if not fst: # first FST
+        fst = f
+      else:
+        fst = fst + f
+
+    if len(stack) > 0:
+      raise Exception('Unmatched brackets')
+
+    return fst.optimize()
