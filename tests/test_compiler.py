551dcfccfd31ba28c65d40553a6000c4565ce16f
Kalvin Chang <kalvin1204@gmail.com>
2021-04-26 23:42:32 +0800

    compiler: add additional tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -23,7 +23,7 @@ def test_single_starting_class_no_continuation():
   
   assert analyze(fst, 'b') == 'a' # direction of morphological analysis
 
-  # FST does not do morphological generation
+  # FST does not do morphological generation (FST rejects upper alphabet symbols)
   with pytest.raises(Exception):
     analyzed(fst, 'a')
 
@@ -140,11 +140,132 @@ def test_multiple_starting_classes_different_continuation():
   with pytest.raises(Exception):
     analyzed(fst, 'h')
 
-def test_multiple_starting_classes_multiple_continuations():
+def test_multiple_starting_classes_single_rule_per_class_multiple_continuations():
   fst = compile({
-    Slot('class1', [('a', 'b', ['class3'], 0.0)], start=True),
+    Slot('class1', [('a', 'b', ['class2', 'class3', 'class4'], 0.0)], start=True),
+    Slot('class2', [('c', 'd', [], 0.0)]),
     Slot('class3', [('e', 'f', [], 0.0)]),
-    Slot('class2', [('c', 'd', ['class4'], 0.0)], start=True),
-    Slot('class4', [('g', 'h', [], 0.0)], start=True)
+    Slot('class4', [('g', 'h', [], 0.0)]),
+    Slot('class5', [('i', 'j', [], 0.0)], start=True)
   })
-  assert analyze(fst, 'dh') == 'cg'
+  assert analyze(fst, 'bd') == 'ac'
+  assert analyze(fst, 'bf') == 'ae'
+  assert analyze(fst, 'bh') == 'ag'
+  assert analyze(fst, 'j') == 'i'
+
+  # multiple continuation classes do not interfere with each other
+  with pytest.raises(Exception):
+    analyzed(fst, 'bfh') # class3 not joined with class4
+  with pytest.raises(Exception):
+    analyzed(fst, 'bdf') # class2 not joined with class3
+  with pytest.raises(Exception):
+    analyzed(fst, 'bdh') # class2 not joined with class4
+
+  # must start with a starting class
+  for non_starting_class_symbol in ['b', 'd', 'f']:
+    with pytest.raises(Exception):
+      analyzed(fst, non_starting_class_symbol)
+
+def test_multiple_rules_single_class_no_continuations():
+  fst = compile({
+    Slot('class1',
+      [
+        ('a', 'b', [], 0.0),
+        ('c', 'd', [], 0.0),
+        ('e', 'f', [], 0.0),
+        ('g', 'h', [], 0.0),
+      ], 
+      start=True),
+  })
+
+  assert analyze(fst, 'b') == 'a'
+  assert analyze(fst, 'd') == 'c'
+  assert analyze(fst, 'f') == 'e'
+  assert analyze(fst, 'h') == 'g'
+
+  # FST does not accept upper alphabet symbols
+  for input_symbol in ['a', 'c', 'e', 'g']:
+    with pytest.raises(Exception):
+      analyzed(fst, input_symbol)
+
+  # a slot is a union of rules, not a concatenation
+  for not_in_lang in ['bd', 'df', 'fh', 'bh', 'dh', 'bf']:
+    with pytest.raises(Exception):
+      analyzed(fst, not_in_lang)
+
+def test_multiple_rules_single_starting_class_with_multiple_continuations():
+  fst = compile({
+    Slot('class1',
+      [
+        ('a', 'b', ['class2', 'class3'], 0.0),
+        ('c', 'd', ['class4'], 0.0),
+        ('e', 'f', [], 0.0),
+        ('g', 'h', [], 0.0)
+      ],
+      start=True),
+    Slot('class2', [('i', 'j', [], 0.0)]),
+    Slot('class3', [('k', 'l', [], 0.0)]),
+    Slot('class4', [('m', 'n', [], 0.0)])
+  })
+
+  assert analyze(fst, 'bj') == 'ai'
+  assert analyze(fst, 'bl') == 'ak'
+  assert analyze(fst, 'dn') == 'cm'
+  assert analyze(fst, 'h') == 'g'
+
+  # rules within a slot should not be concatenated with wrong continuation class
+  for not_in_lang in ['bf', 'bh', 'bd', 'bn', 'df', 'dh', 'db', 'dj', 'dl']:
+    with pytest.raises(Exception):
+      analyzed(fst, not_in_lang)
+
+def test_multiple_rules_multiple_classes_multiple_continuations():
+  fst = compile({
+    Slot('class1',
+      [
+        ('a', 'b', ['class2'], 0.0),
+        ('c', 'd', [], 0.0),
+        ('e', 'f', ['class2', 'class3'], 0.0)
+      ],
+      start=True),
+    Slot('class2', 
+      [
+        ('g', 'h', [], 0.0),
+        ('i', 'j', [], 0.0),
+        ('k', 'l', ['class3'], 0.0),
+      ]
+    ),
+    Slot('class3', 
+      [
+        ('m', 'n', [], 0.0),
+        ('o', 'p', [], 0.0),
+      ]
+    ),
+    Slot('class4', 
+      [
+        ('q', 'r', [], 0.0),
+        ('s', 't', [], 0.0),
+      ], start=True)
+  })
+  
+  # class1 alone
+  assert analyze(fst, 'b') == 'a'
+
+  # class1 to class2
+  assert analyze(fst, 'bh') == 'ag'
+  assert analyze(fst, 'bj') == 'ai'
+  assert analyze(fst, 'fh') == 'eg'
+  assert analyze(fst, 'fj') == 'ei'
+
+  # class1 to class2 to class3
+  assert analyze(fst, 'bln') == 'akm'
+  assert analyze(fst, 'blp') == 'ako'
+  assert analyze(fst, 'fln') == 'ekm'
+  assert analyze(fst, 'flp') == 'eko'
+
+  # class1 to class3
+  assert analyze(fst, 'fn') == 'em'
+  assert analyze(fst, 'fp') == 'eo'
+
+  # class4
+  assert analyze(fst, 'r') == 'q'
+  assert analyze(fst, 't') == 's'
